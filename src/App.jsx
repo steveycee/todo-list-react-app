dec46fb6d185bb27b8b0ad3bdf4af23cde1b5873
Steven Chapman <stevey.cee@gmail.com>
2024-08-05 17:08:50 +0100

    chore: updating some naming

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-
-import './App.css'
-import ToDoList from './components/todolist'
+import "./App.css";
+import ToDoList from "./components/todo-list";
 
 function App() {
-
-  return (
-    <>
-      <h1>Todo List App</h1>
-      < ToDoList />
-    </>
-  )
+	return (
+		<>
+			<h1>Todo List App</h1>
+			<ToDoList />
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/todo-list-item.jsx
@@ -0,0 +1,14 @@
+import "./todo-list-item.css";
+
+function ListItem() {
+	return (
+		<>
+			<div className="listitem">
+				<h3>Test ListItem</h3>
+				<button>X</button>
+			</div>
+		</>
+	);
+}
+
+export default ListItem;

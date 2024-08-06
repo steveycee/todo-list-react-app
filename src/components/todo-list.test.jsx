401e2519fb3d887ffd6c71ce4942e3bd6c550bb9
Steven Chapman <stevey.cee@gmail.com>
2024-08-06 17:35:49 +0100

    test: expandedf on todolist test
    Co-Authored-By: Sarah Norris <1645628+mikachan@users.noreply.github.com>

--- src/components/todo-list.test.jsx
+++ src/components/todo-list.test.jsx
@@ -2,9 +2,17 @@ import { render, screen } from "@testing-library/react";
 import ToDoList from "./todo-list";
 
 describe("ToDoList", () => {
-	it("renders the ToDoList component", () => {
+	it("renders the ToDoList component and expects a H2", () => {
 		render(<ToDoList />);
-		expect(screen.getByText("Test ToDoList")).toBeInTheDocument();
+		expect(screen.getByRole("heading"), {
+			level: 2,
+		}).toBeInTheDocument();
+	});
+	it("renders the ToDoList component and finds the add list item button", () => {
+		render(<ToDoList />);
+		expect(
+			screen.getByRole("button", { name: "Add list item" })
+		).toBeInTheDocument();
 		screen.debug();
 	});
 });

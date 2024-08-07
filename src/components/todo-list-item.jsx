8ed9f456f6542e0882061407e23aeb13cc8c8277
Steven Chapman <stevey.cee@gmail.com>
2024-08-07 17:33:34 +0100

    feat: adding input to todo list items

--- src/components/todo-list-item.jsx
+++ src/components/todo-list-item.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import "./todo-list-item.css";
 
 function TodoListItem({ handleRemoveListItem }) {
+	const [listItemsContent, setListItemsContent] = useState([]);
+
 	return (
 		<>
 			<li className="listitem">
-				<p>Test ListItem</p>
+				<form>
+					<input
+						type="text"
+						placeholder="Write your todo here"
+						// ref={inputRef}
+					/>
+				</form>
+				<button>✓</button>
+
 				<button
 					onClick={() => {
 						handleRemoveListItem();

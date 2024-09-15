5a6d898ccbbdfa86306a23c8889cf5babb578c50
Steve Chapman <stevey.cee@gmail.com>
2024-09-15 14:47:56 +0100

    tests: added test for remove listitem

--- src/components/todo-list.test.jsx
+++ src/components/todo-list.test.jsx
@@ -10,17 +10,17 @@ describe("Todo list tests", () => {
 		submitButton = screen.getByRole("button", { name: "✓" });
 	});
 
-	it("renders the ToDoList component and the todolist to have a heading", () => {
+	it("renders the ToDoList component and the todolist to have a heading.", () => {
 		expect(screen.getByRole("heading")).toHaveTextContent("Test ToDoList");
 		screen.debug();
 	});
-	it("renders the ToDoList component and finds the submit button", () => {
+	it("renders the ToDoList component and finds the submit button.", () => {
 		expect(submitButton).toBeInTheDocument();
 	});
-	it("renders the ToDoList component and finds the submit button", () => {
+	it("renders the ToDoList component and finds the submit button.", () => {
 		expect(submitButton).toBeInTheDocument();
 	});
-	it("renders the ToDoList component and types in the input field", () => {
+	it("renders the ToDoList component and types in the input field.", () => {
 		const input = screen.getByRole("textbox");
 		fireEvent.change(input, { target: { value: "Steve" } });
 		expect(input).toHaveValue("Steve");
@@ -31,5 +31,22 @@ describe("Todo list tests", () => {
 		const todosListItem = screen.getAllByRole("listitem");
 		expect(todosListItem).toHaveLength(2);
 		expect(todosListItem[1]).toBeInTheDocument();
+	});
+	it("renders the ToDoList component, adds a todo and then removes the todo.", () => {
+		// create a todo item
+		const input = screen.getByRole("textbox");
+		fireEvent.change(input, { target: { value: "Steve" } });
+		expect(input).toHaveValue("Steve");
+		fireEvent.click(submitButton);
+		const todosListItem = screen.getAllByRole("listitem");
+		expect(todosListItem).toHaveLength(1);
+		// remove a todo item
+		const removeButton = screen.getByRole("button", { name: "Remove" });
+		expect(removeButton).toBeInTheDocument();
+		fireEvent.click(removeButton);
+		// couldnt recheck for todoListItem as that array is already initialised above. Can't check it again because now there isn't any. What I can do is query that its there and then expect it not to be.
+		const updatedTodosListItem = screen.queryByRole('listitem')
+		expect(updatedTodosListItem).toBeNull()
+		screen.debug();
 	});
 });

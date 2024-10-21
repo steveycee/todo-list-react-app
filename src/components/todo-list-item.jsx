276e0a16a5d49914dc7e30c08deb4c2952b6a363
Steve Chapman <stevey.cee@gmail.com>
2024-10-21 21:50:42 +0100

    refactor: todolistitem from fragment to div

--- src/components/todo-list-item.jsx
+++ src/components/todo-list-item.jsx
@@ -3,14 +3,15 @@ import "./todo-list.css";
 function ToDoListItem({ todos, removeListItem, markListItemAsDone }) {
 
     return (
-            <>
+            <div>
                 <li key={todos.id} className="flex-row border">
                     Todos is: {todos.content} with an id of {todos.id}
                 </li>
                 <button onClick={() => markListItemAsDone(todos.id)}>Done</button>
                 <button onClick={() => removeListItem(todos.id)}>Remove</button>
                 <br />
-            </>
+            </div>
   );
 }
+
 export default ToDoListItem;

1c116a12391b925a210445b7a2dc19ff300d724a
Steve Chapman <stevey.cee@gmail.com>
2024-10-21 19:58:55 +0100

    refactor: abstract todo-list-item to own component
    Solved issue of item not appearing to have a unique key

--- src/components/todo-list-display.jsx
+++ src/components/todo-list-display.jsx
@@ -1,18 +1,13 @@
 import "./todo-list.css";
+import ToDoListItem from "./todo-list-item";
+
 
 function ToDoListDisplay({ todos, removeListItem, markListItemAsDone }) {
   return (
     <div className="todo-list-item">
         <ul>
         {todos.map((todos) => (
-            <>
-            <li key={todos.id} className="flex-row border">
-                Todos is: {todos.content} with an id of {todos.id}
-            </li>
-            <button onClick={() => markListItemAsDone(todos.id)}>Done</button>
-            <button onClick={() => removeListItem(todos.id)}>Remove</button>
-            <br />
-            </>
+            <ToDoListItem key={todos.id} todos={todos} removeListItem={removeListItem} markListItemAsDone={markListItemAsDone} />
         ))}
         </ul>
   </div>
---
+++ src/components/todo-list-item.jsx
@@ -0,0 +1,15 @@
+import "./todo-list.css";
+
+function ToDoListItem({ todos, removeListItem, markListItemAsDone }) {
+  return (
+            <>
+                <li key={todos.id} className="flex-row border">
+                    Todos is: {todos.content} with an id of {todos.id}
+                </li>
+                <button onClick={() => markListItemAsDone(todos.id)}>Done</button>
+                <button onClick={() => removeListItem(todos.id)}>Remove</button>
+                <br />
+            </>
+  );
+}
+export default ToDoListItem;
